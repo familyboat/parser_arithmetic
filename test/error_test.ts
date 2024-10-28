a150396c01e4740ed5ed9d41c8a7a2e83efc3977
familyboat <2015301110129@whu.edu.cn>
2024-10-28 15:26:15 +0800

    添加README.md 和 新的测试用例

--- test/error_test.ts
+++ test/error_test.ts
@@ -2,11 +2,41 @@ import { strict as assert } from "node:assert";
 import { Context } from "../src/context.ts";
 
 Deno.test({
+  name: "00",
+  fn() {
+    const text = "00";
+    const context = new Context(text);
+    assert.throws(
+      context.parse,
+      ` Error: 解析zero过程中出现错误\n00\n~`,
+    );
+  },
+});
+
+Deno.test({
   name: "0 123",
   fn() {
     const text = "0 123";
     const context = new Context(text);
     assert.throws(context.parse, `Error: 解析zero过程中出现错误\n0 123\n~`);
+  },
+});
+
+Deno.test({
+  name: "0(",
+  fn() {
+    const text = "0(";
+    const context = new Context(text);
+    assert.throws(context.parse, `Error: 解析zero过程中出现错误\n0(\n~`);
+  },
+});
+
+Deno.test({
+  name: "(0)",
+  fn() {
+    const text = "(0)";
+    const context = new Context(text);
+    assert.throws(context.parse, `Error: 解析signedInteger过程中出现错误\n(0)\n~`);
   },
 });
 
