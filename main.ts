9d561ce169ce8a23139d325c9019451097b31cdc
familyboat <2015301110129@whu.edu.cn>
2024-10-28 15:02:34 +0800

    为发布做准备

--- main.ts
+++ main.ts
@@ -1,10 +1,18 @@
 import { Context } from "./src/context.ts";
 
 if (import.meta.main) {
-  const text = "1+(+23)+1+(-22)+1*(+2)+2/(-1)";
-  const ast = new Context(text).parseAst();
-  const pretty = ast?.pretty();
-  const result = ast?.evaluate();
-  console.log(`输入是：${text}`);
-  console.log(`输出是：${pretty} = ${result}`);
+  const text = Deno.args[0] || "1+2";
+
+  const help = `算数解析器，支持加减乘除四种运算，支持无符号的整数，支持有符号的整数（需包裹在圆括号内）\n示例：1+(-2)-3*(+4)+5/(-1)`
+  
+  try {
+    const ast = new Context(text).parseAst();
+    const pretty = ast?.pretty();
+    const result = ast?.evaluate();
+    console.log(`输入是：${text}`);
+    console.log(`输出是：${pretty} = ${result}`);
+  } catch (error: unknown) {
+    console.log(`${error}`);
+    console.log(help);
+  }
 }
